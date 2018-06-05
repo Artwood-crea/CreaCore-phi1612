737840b1a13d6c003e4aa470e49140bb6a424187
Artwood Crea Coin <artwood@creacoin.info>
2018-06-05 15:29:51 +0300

    New Genesis block implementation
    We starting it today

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -7,7 +7,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "chainparams.h"
- 
+
 #include "random.h"
 #include "util.h"
 #include "utilstrencodings.h"
@@ -25,6 +25,29 @@ struct SeedSpec6 {
 };
 
 #include "chainparamsseeds.h"
+void MineGenesis(CBlock genesis){
+    // This will figure out a valid hash and Nonce if you're creating a different genesis block:
+    uint256 newhash = genesis.GetHash();
+    uint256 besthash;
+    int64_t nStart = GetTime();
+    uint256 hashTarget = ~uint256(0) >> 20;
+    printf("Target: %s\n", hashTarget.GetHex().c_str());
+    memset(&besthash,0xFF,32);
+    while (newhash > hashTarget) {
+        ++genesis.nNonce;
+        if (genesis.nNonce == 0){
+            printf("NONCE WRAPPED, incrementing time");
+            ++genesis.nTime;
+        }
+        newhash = genesis.GetHash();
+        if(newhash < besthash){
+            besthash=newhash;
+            printf("New best: %s\n", newhash.GetHex().c_str());
+        }
+    }
+    printf("Found Genesis, Nonce: %ld \nHash: %s\n", genesis.nNonce, genesis.GetHash().GetHex().c_str());
+    printf("Genesis Hash Merkle: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+}
 
 
 /**
@@ -55,21 +78,15 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 
 static Checkpoints::MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-                (0, uint256("0x00000aec3ce04601f5a475eab9f30dfccfb1eb9076a9c6c853e07a7734809c98"))
-		        (10, uint256("0x000004df093d73d11068eb52f61572bf457b0925aa78f74ab2287832be42fc12"))
-		        (500, uint256("0x00000000000b9160602ee8949d8c5cdffb5cb261ee4a66add0ca1686815cf54c"))
-		        (5000, uint256("0x00000000001044f5653026712e9af65c74a4a0878afb848de05e458625c8187a"))
-		        (7200, uint256("0x0000000000057bd12ed10785d6636299dc4840360b40f3126b59e9f997e069ea"))
-		        (10802, uint256("0x0000000000033d9285e516da50779f81268240047904c4e1377c5ad39ec9d6ac"))
-		        (15425, uint256("0x0000000000003b88cd7ae706ad099d7854e43ce6c591c86c6c8ec241d4560525"));
+                (0, uint256("0x0000071b20be7ce95873bb629184195ec550296f6f134c80637e57557591574b"));
 
 
 static const Checkpoints::CCheckpointData data = {
         &mapCheckpoints,
-        1520926214,// * UNIX timestamp of last checkpoint block
-        23067,    // * total number of transactions between genesis and last checkpoint
+        1528197137,// * UNIX timestamp of last checkpoint block
+        1,    // * total number of transactions between genesis and last checkpoint
         //   (the tx=... number in the SetBestChain debug.log lines)
-        2000        // * estimated number of transactions per day after checkpoint
+        1        // * estimated number of transactions per day after checkpoint
 };
 
 static Checkpoints::MapCheckpoints mapCheckpointsTestnet =
@@ -98,12 +115,12 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
-        pchMessageStart[0] = 0x66;
-        pchMessageStart[1] = 0x6f;
-        pchMessageStart[2] = 0x6c;
+        pchMessageStart[0] = 0x62;
+        pchMessageStart[1] = 0x6a;
+        pchMessageStart[2] = 0x6b;
         pchMessageStart[3] = 0x6d;
-        vAlertPubKey = ParseHex("042da06568454d47e0b2349c6953b069690d13fd501e081ee8679cf4a0daaacb7c131f640048df4275a9fa172b34f12349856624031f76a56096aa1a71fe47b66f");
-        nDefaultPort = 53656;
+        vAlertPubKey = ParseHex("042f1c1963c1efff3b846209dd9433d2c67347c31e2163198e896caeaa7fa887449bc59fc267623b16e446f494d06c3bcf65a46babdb6e87560b60fcc7199971c8");
+        nDefaultPort = 52640;
         bnProofOfWorkLimit = ~uint256(0) >> 20; // Folm starting difficulty is 1 / 2^12
         nMaxReorganizationDepth = 100;
         nMinerThreads = 0;
@@ -112,47 +129,39 @@ public:
         nTargetSpacing = 2* 60; // Folm: 2 mins
         nLastPOWBlock = 26280000; // 100 Years
         nRampToBlock = 500; // Slow start, ramp linearly to this block
-        nMaturity = 49; // 98 Minutes
+        nMaturity = 60; // 120 Minutes
         nMasternodeCountDrift = 4;
         nModifierUpdateBlock = 1;
-        nMaxMoneyOut = 22739116 * COIN; // Year 2050
+        nMaxMoneyOut = 25000000 * COIN; // Year 2113, spring
 
-        const char* pszTimestamp = "17 February 2018 - Folm Coin - Week of chaos, again, engulfs Trump";
+        const char* pszTimestamp = "Creative Innovations Coin (CIC) started on 5 June 2018";
         CMutableTransaction txNew;
         txNew.nVersion = 1;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 0 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("040e3fe58595206cf8c5612c977e10335393b689ead98cda8c0e4e29e5377b628ff6de54ca1936ac57072ce70968ee5202b91ddd53bc4f8f2baa8640") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("04dfab48e49169da8dd843d67a836e088a2ab91b9139d8a5449b5f703270da001db34f56774c2267dc2fa7d22def8166a1fe8a2b331dad89479f52853aece7bfd2") << OP_CHECKSIG;
         genesis.vtx.push_back(txNew);
         genesis.hashPrevBlock.SetNull();
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime = 1518900000;
+        genesis.nTime = 1528197137;
         genesis.nBits = bnProofOfWorkLimit.GetCompact();
-        genesis.nNonce = 1809743;
+        genesis.nNonce = 436303;
 
         hashGenesisBlock = genesis.GetHash();
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        assert(hashGenesisBlock == uint256("0x00000aec3ce04601f5a475eab9f30dfccfb1eb9076a9c6c853e07a7734809c98"));
-        assert(genesis.hashMerkleRoot == uint256("0x985190fcf993fea0ab74ca2e9cc59300469055edd73512ef23bb01cce0aaea81"));
+        assert(hashGenesisBlock == uint256("0x0000071b20be7ce95873bb629184195ec550296f6f134c80637e57557591574b"));
+        assert(genesis.hashMerkleRoot == uint256("0xc6c0a85fbc8a48701ae0ba603d4ae025af00c2bc91a9aece07413480d83b2523"));
 
-        vSeeds.push_back(CDNSSeedData("folm-node10.folmcoin.com", "folm-node10.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node09.folmcoin.com", "folm-node09.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node08.folmcoin.com", "folm-node08.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node07.folmcoin.com", "folm-node07.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node06.folmcoin.com", "folm-node06.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node05.folmcoin.com", "folm-node05.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node04.folmcoin.com", "folm-node04.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node03.folmcoin.com", "folm-node03.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node02.folmcoin.com", "folm-node02.folmcoin.com"));
-        vSeeds.push_back(CDNSSeedData("folm-node01.folmcoin.com", "folm-node01.folmcoin.com"));
+        vSeeds.push_back(CDNSSeedData("dnss1.creacoin.info", "dnss1.creacoin.info"));
+        vSeeds.push_back(CDNSSeedData("dnss2.creacoin.info", "dnss2.creacoin.info"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 36); // Folm address start with F
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 36);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 28); // Folm address start with F
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 28);
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1, 178);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x15)(0x2A)(0x21)(0x13).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x03)(0x33)(0x4C)(0xAF).convert_to_container<std::vector<unsigned char> >();
@@ -172,9 +181,9 @@ public:
         fHeadersFirstSyncingActive = false;
 
         nPoolMaxTransactions = 3;
-        strSporkKey = "0425a68d2a8b1f375f69926ff68c8350982d2890984d4b09e118cc29e6359ae1caf0669ecef05b671fb75b8452cb30a7d36d09ccb9693988510fc547ee45c904ce";
+        strSporkKey = "0434273b4ec9c9111cd9ca592281e001f6737e18fbe63d2024f17873ba9d8ba3bff040c52e1bdb46c703c3baa62f4d7e8aa1f1f26d32c8ef737919762fd18e4c9e";
         strObfuscationPoolDummyAddress = "FZS69ngaE5dAgT3K6mGTu4AmrNwFB1si6r";
-        nStartMasternodePayments = 1518900000;
+        nStartMasternodePayments = 1528416000;
     }
 
 
@@ -217,9 +226,9 @@ public:
         genesis.hashMerkleRoot == uint256("0x");
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("folm.io", "test01.folm.io"));
-        vSeeds.push_back(CDNSSeedData("folm.io", "test02.folm.io"));
-        vSeeds.push_back(CDNSSeedData("folm.io", "test03.folm.io"));
+        vSeeds.push_back(CDNSSeedData("creacoin.info", "test01.creacoin.info"));
+        vSeeds.push_back(CDNSSeedData("creacoin.info", "test02.creacoin.info"));
+
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 65); // Testnet folm addresses start with 'T'
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 12);  // Testnet folm script addresses start with '5' or '6'
